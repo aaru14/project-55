418b45a964f86413b43c0833d6cc2c8f932f2047
aaru14 <82047044+aaru14@users.noreply.github.com>
2021-07-25 13:21:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function one(){
+    window.alert("This is Taj Mahal located in India")
+}
+function two(){
+    window.alert("This is th Roman Colosseum found in Italy")
+}
+function three(){
+    window.alert("The great wall of China")
+}
+function four(){
+    window.alert("Chichén Itzá El Castillo, a Toltec-style pyramid, Chichén Itzá, Yucatán state, Mexico.")
+}
+function five(){
+    window.alert("petra. the Khaznah.")
+}
+function six(){
+    window.alert("Machu Picchu. Machu Picchu, Peru.")
+}
+function seven(){
+    window.alert("Christ the Redeemer. Christ the Redeemer statue")
+}
